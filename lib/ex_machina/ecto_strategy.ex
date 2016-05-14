06994183d22bba4503ba2051bbca47eb4bc9373b
Josh Steiner <josh@jsteiner.me>
2016-05-14 18:35:00 -0400

    Cast all values before insert
    Casts each field based on the type declared in the schema. Recursively
    casts built associations.

--- lib/ex_machina/ecto_strategy.ex
+++ lib/ex_machina/ecto_strategy.ex
@@ -13,7 +13,9 @@ defmodule ExMachina.EctoStrategy do
   end
 
   def handle_insert(%{__meta__: %{__struct__: Ecto.Schema.Metadata}} = record, %{repo: repo}) do
-    repo.insert! record
+    record
+    |> cast
+    |> repo.insert!
   end
 
   def handle_insert(record, %{repo: _repo}) do
@@ -22,5 +24,66 @@ defmodule ExMachina.EctoStrategy do
 
   def handle_insert(_record, _opts) do
     raise "expected :repo to be given to ExMachina.EctoStrategy"
+  end
+
+  defp cast(record) do
+    record
+    |> cast_fields
+    |> cast_assocs
+  end
+
+  defp cast_fields(struct) do
+    struct
+    |> ExMachina.Ecto.drop_ecto_fields
+    |> Map.keys
+    |> cast_fields(struct)
+  end
+
+  defp cast_fields(fields, struct) do
+    Enum.reduce(fields, struct, fn(field, struct) ->
+      casted_value = cast_field(field, struct)
+      Map.put(struct, field, casted_value)
+    end)
+  end
+
+  defp cast_field(field, %{__struct__: schema} = struct) do
+    field_type = schema.__schema__(:type, field)
+    value = Map.get(struct, field)
+
+    if field_type do
+      cast_value(field_type, value, struct)
+    else
+      value
+    end
+  end
+
+  defp cast_value(field_type, value, struct) do
+    case Ecto.Type.cast(field_type, value) do
+      {:ok, value} ->
+        value
+      _ ->
+        raise "Failed to cast `#{value}` of type #{field_type} in #{inspect struct}."
+    end
+  end
+
+  defp cast_assocs(%{__struct__: schema} = struct) do
+    assocs = schema.__schema__(:associations)
+
+    Enum.reduce(assocs, struct, fn(assoc, struct) ->
+      original_value = Map.get(struct, assoc)
+      casted_value = cast_assoc(original_value)
+      Map.put(struct, assoc, casted_value)
+    end)
+  end
+
+  defp cast_assoc(record) do
+    case record do
+      %{__meta__: %{__struct__: Ecto.Schema.Metadata, state: :built}} ->
+        cast(record)
+      records = [_ | _] ->
+        Enum.map(records, &(cast(&1)))
+      _ ->
+        record
+    end
   end
 end
